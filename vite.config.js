4bb560c016f44193d1db4a85bcb6e4e3a826ce1e
JimyMPFN <jzavalar@mpfn.gob.pe>
2026-03-09 16:58:31 -0500

    ModuloLogin05

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app-adminlte.css', 'resources/js/app-adminlte.js'],
-            refresh: true,
+        input: [
+            'resources/css/app.css',           // tu CSS principal (login, etc.)
+            'resources/css/app-adminlte.css',  // tu CSS de AdminLTE
+            'resources/js/app.js',             // JS principal
+            'resources/js/app-adminlte.js'     // JS de AdminLTE si existe
+        ],
+        refresh: true,  // recarga automática al cambiar Blade o CSS/JS
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
     },
-});
+    });
